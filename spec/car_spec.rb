d360ad85b3fa0855fffcd048b6487c7c1fb6a0eb
jkr <abduqahhor.muhamadjonov@gmail.com>
2019-03-17 22:50:02 +0500

    fc

---
+++ spec/car_spec.rb
@@ -0,0 +1,54 @@
+require 'car'
+
+describe 'Car' do
+  describe 'attributes' do
+    # put 'x' befor 'it' => 'xit' in order to skip the test
+    it 'allows reading and writing for :make' do
+      # just write 'skip' at the first line of the method to skip the test
+      # or skip('Reason - message')
+      car = Car.new
+      car.make = 'Test'
+      expect(car.make).to eq('Test')
+    end
+
+    it 'allows reading and writing for :year' do
+      car = Car.new
+      car.year = 9999
+      expect(car.year).to eq(9999)
+    end
+
+    it 'allows reading and writing for :color' do
+      car = Car.new
+      car.color = 'Gogo'
+      expect(car.color).to eq('Gogo')
+    end
+
+    it 'allows reading for :wheels' do
+      car = Car.new
+      expect(car.wheels).to eq(4)
+    end
+
+    it 'allows writing for :doors'
+  end
+
+  describe '.colors' do
+    it 'returns an array of colors' do
+      colors = %w[blue black red green]
+      expect(Car.colors).to match_array(colors)
+    end
+  end
+
+  describe '#full_name' do
+    it 'returns string in expected format' do
+      my_bach = Car.new(make: 'Mybach', year: 2019, color: 'black')
+      expect(my_bach.full_name).to eq('2019 Mybach (black)')
+    end
+
+    context 'when initialized without arguments' do
+      it 'return string using default value' do
+        car = Car.new
+        expect(car.full_name).to eq('2007 Volvo (unknown)')
+      end
+    end
+  end
+end
