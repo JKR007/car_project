5612d64ae817568c045b84bcb7b43f1e378df9d8
jkr <abduqahhor.muhamadjonov@gmail.com>
2019-03-31 16:01:56 +0500

    use #let to initialzize instances or vars DRY

--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -3,30 +3,33 @@ require 'car'
 describe 'Car' do
   describe 'attributes' do
 
-    before(:example) do
-      @car = Car.new
-    end
+    let(:car) { Car.new }
+    # lazy-execution and memoization
+    # "let" is better than "before"
+    # before(:example) do
+    #   car = Car.new
+    # end
 
     # put 'x' befor 'it' => 'xit' in order to skip the test
     it 'allows reading and writing for :make' do
       # just write 'skip' at the first line of the method to skip the test
       # or skip('Reason - message')
-      @car.make = 'Test'
-      expect(@car.make).to eq('Test')
+      car.make = 'Test'
+      expect(car.make).to eq('Test')
     end
 
     it 'allows reading and writing for :year' do
-      @car.year = 9999
-      expect(@car.year).to eq(9999)
+      car.year = 9999
+      expect(car.year).to eq(9999)
     end
 
     it 'allows reading and writing for :color' do
-      @car.color = 'Gogo'
-      expect(@car.color).to eq('Gogo')
+      car.color = 'Gogo'
+      expect(car.color).to eq('Gogo')
     end
 
     it 'allows reading for :wheels' do
-      expect(@car.wheels).to eq(4)
+      expect(car.wheels).to eq(4)
     end
 
     it 'allows writing for :doors'
