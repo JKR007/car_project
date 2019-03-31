f47bb37431bc0a456b1e79842b2dc2ae142e84fd
jkr <abduqahhor.muhamadjonov@gmail.com>
2019-03-31 16:14:00 +0500

    use #subject if instnce is a subject of example - instead #let

--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -2,10 +2,14 @@ require 'car'
 
 describe 'Car' do
   describe 'attributes' do
+    # use "subject" instead of "let"
+    # if variable of is subject of example
+    subject { Car.new }
 
-    let(:car) { Car.new }
+    # let(:car) { Car.new }
     # lazy-execution and memoization
     # "let" is better than "before"
+
     # before(:example) do
     #   car = Car.new
     # end
@@ -14,43 +18,43 @@ describe 'Car' do
     it 'allows reading and writing for :make' do
       # just write 'skip' at the first line of the method to skip the test
       # or skip('Reason - message')
-      car.make = 'Test'
-      expect(car.make).to eq('Test')
+      subject.make = 'Test'
+      expect(subject.make).to eq('Test')
     end
 
     it 'allows reading and writing for :year' do
-      car.year = 9999
-      expect(car.year).to eq(9999)
+      subject.year = 9999
+      expect(subject.year).to eq(9999)
     end
 
     it 'allows reading and writing for :color' do
-      car.color = 'Gogo'
-      expect(car.color).to eq('Gogo')
+      subject.color = 'Gogo'
+      expect(subject.color).to eq('Gogo')
     end
 
     it 'allows reading for :wheels' do
-      expect(car.wheels).to eq(4)
+      expect(subject.wheels).to eq(4)
     end
 
     it 'allows writing for :doors'
   end
 
   describe '.colors' do
+    let(:colors) { %w[blue black red green] }
     it 'returns an array of colors' do
-      colors = %w[blue black red green]
       expect(Car.colors).to match_array(colors)
     end
   end
 
   describe '#full_name' do
+    let(:my_bach) { Car.new(make: 'Mybach', year: 2019, color: 'black') }
     it 'returns string in expected format' do
-      my_bach = Car.new(make: 'Mybach', year: 2019, color: 'black')
       expect(my_bach.full_name).to eq('2019 Mybach (black)')
     end
 
     context 'when initialized without arguments' do
+      let(:car) { Car.new }
       it 'return string using default value' do
-        car = Car.new
         expect(car.full_name).to eq('2007 Volvo (unknown)')
       end
     end
