b14a5af45498b6eaaa8db9e9ab1cc65b94fce1c8
jkr <abduqahhor.muhamadjonov@gmail.com>
2019-04-01 13:44:17 +0500

    DRY - move common spec to shared example/a_standard_vehicle

--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -1,46 +1,9 @@
 require 'car'
+require 'shared_examples/a_standard_vehicle'
 
 describe Car do
-  describe 'attributes' do
-    # do not need to defince subject {  } explicitly
-    # if we use a #className after #describe
 
-    # use "subject" instead of "let"
-    # if variable of is subject of example
-    # subject { Car.new }
-
-    # let(:car) { Car.new }
-    # lazy-execution and memoization
-    # "let" is better than "before"
-
-    # before(:example) do
-    #   car = Car.new
-    # end
-
-    # put 'x' befor 'it' => 'xit' in order to skip the test
-    it 'allows reading and writing for :make' do
-      # just write 'skip' at the first line of the method to skip the test
-      # or skip('Reason - message')
-      subject.make = 'Test'
-      expect(subject.make).to eq('Test')
-    end
-
-    it 'allows reading and writing for :year' do
-      subject.year = 9999
-      expect(subject.year).to eq(9999)
-    end
-
-    it 'allows reading and writing for :color' do
-      subject.color = 'Gogo'
-      expect(subject.color).to eq('Gogo')
-    end
-
-    it 'allows reading for :wheels' do
-      expect(subject.wheels).to eq(4)
-    end
-
-    it 'allows writing for :doors'
-  end
+  it_behaves_like('a standard vehicle')
 
   describe '.colors' do
     let(:colors) { %w[blue black red green] }
